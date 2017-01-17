c6271cd9e0bd4baf59dfe03bf8b60ee05a683500
Sam <sam.werngren@gmail.com>
2017-01-17 15:23:20 +0000

    connection working

--- server.rb
+++ server.rb
@@ -1,10 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+#delete if not using postgres
+#require 'pg'
 require 'haml'
 require 'csv'
 require 'mongo'
 
-db = Mongo::Connection.new.db("mydb")
+client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'mydb')
+db = client.database
+
+#db = Mongo::Connection.new.db("mydb")
 
 get '/' do
   haml :home
@@ -19,11 +24,14 @@ end
 post '/upload' do
   file_data = params[:myfile][:tempfile].read
   csv_rows  = CSV.parse(file_data, headers: true, header_converters: :symbol)
-  remarks = 'none yet'
+  #remarks = 'none yet'
   csv_rows.each do |row| 
   	#put in params
       #DB[:prospects].insert( :Id =>  row[:Id], :Title => row[:title] )
-      #link up mongodb
+      db[:mydb].insert_one(row)
+      #or it might be this to insert all the data
+      #DB[:mydb].insert_many(csv_rows)
+      
     end
   return "File successfully uploaded"
 end
